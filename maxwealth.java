28ee79f2a6caeb78b2781804767d659ece972167
yokesh-kumar <yokeshkumar1704@gmail.com>
2025-07-24 00:09:11 +0530

    23-07-25

---
+++ maxwealth.java
@@ -0,0 +1,40 @@
+public class maxwealth {
+    // static int maximumWealth(int[][] accounts) {
+    //     int value = 0;
+    //     for(int row=0; row<accounts.length; row++){
+    //         int rowsum = 0;
+    //         for(int col=0; col<accounts[row].length; col++){
+    //             rowsum += accounts[row][col];
+    //         }
+    //         if(rowsum > value){
+    //             value = rowsum;
+    //         }
+    //     }
+    //     return value;
+    // }
+
+    static int maximumWealth(int[][] accounts){
+        int value = Integer.MIN_VALUE;
+        for(int[] i : accounts){
+            int sum = 0;
+            for(int j : i){
+                sum += j;
+            }
+        if(sum > value){
+            value = sum;
+        }
+        }
+        return value;
+    }
+    public static void main(String[] args) {
+        int[][] arr = {
+            {1,2,3},
+            {3,2,1}
+        };
+        int[][]  arr1 = {
+            {1,5},{7,3},{3,5}
+        };
+
+        System.out.println(maximumWealth(arr1));
+    }
+}
