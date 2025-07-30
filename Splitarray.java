2ade4a35ba4b84fb20ba14e99a688f30a290bb65
yokesh-kumar <yokeshkumar1704@gmail.com>
2025-07-30 23:06:32 +0530

    Dummy Commit

--- Splitarray.java
+++ Splitarray.java
@@ -1,7 +1,7 @@
-import java.nio.channels.Pipe.SourceChannel;
-
 public class Splitarray {
+    // this is a dumy push
     public static void main(String[] args) {
+        // Binary search = O(log(N))
         int[] arr = {7,2,5,10,8};
         int k = 2;
 
@@ -9,9 +9,11 @@ public class Splitarray {
         System.out.println(ans);
     }
     static int splitArraySum(int[] arr, int k){
+        // Binary search
         int start = 0;
         int end = 0;
-
+        
+        // For defining the start and end points
         for(int i=0; i<arr.length; i++){
             start = Math.max(start, arr[i]);
             end += arr[i];
@@ -23,15 +25,19 @@ public class Splitarray {
             int sum = 0;
             int pieces = 1;
             for(int num : arr){
+                // Checking whether sum and arr[i] are exceeding mid value
                 if(sum+num > mid){
                     sum = num;
                     pieces++;
+                // If exceeds then fill sum with the particular arr[i] value
                 } else{
                     sum += num;
                 }
             }
+            // If the division is greater than 2 then we point the start value to mid + 1
             if(pieces > k){
                 start = mid + 1;
+            // Else the mid point the assigned to the end value.
             } else{
                 end = mid;
             }
