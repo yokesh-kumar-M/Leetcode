eee97e7fd3db03fc7fdc3cec5d9f83f2fa1765a7
yokesh-kumar-M <yokeshkumar1704@gmail.com>
2025-12-22 15:22:49 +0530

    Add Digit

--- Adddigit.java
+++ Adddigit.java
@@ -1,8 +1,10 @@
 public class Adddigit {
+    // 258 - Add Digit
     public static void main(String[] args) {
         int num = 38;
         System.out.println(addDigit(num));
     }
+    // https://leetcode.com/problems/add-digits/submissions/1862258899/?envType=problem-list-v2&envId=prshgx6i
     static int addDigit(int num){
         if(num < 10){
             return num;
@@ -15,4 +17,16 @@ public class Adddigit {
 
         return addDigit(sum);
     }
+
+    /**
+    static int addDigits(int num){
+        if(num == 0){
+            return 0;
+        } else if(num%9 == 0){
+            return 9; 
+        } else{
+            return num%9;    
+        }
+    } 
+    **/
 }
