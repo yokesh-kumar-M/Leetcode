ded871fbfd602d07ddc95e1382fd6e26622bc374
yokesh-kumar-M <yokeshkumar1704@gmail.com>
2025-12-27 14:05:40 +0530

    Element and Digit difference

--- DigitElement.java
+++ DigitElement.java
@@ -1,8 +1,28 @@
+// 2535. Difference Between Element Sum and Digit of an Array.
 public class DigitElement {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4};
+        int[] arr = {1, 15, 6, 3};
         System.out.println(differenceOfSum(arr));
     }
+
+    static int differenceOfSum(int[] arr){
+        int elementSum = 0;
+        int digitSum = 0;
+
+
+        for(int i : arr){
+            elementSum += i;
+
+            int temp = i;
+            while(temp > 0){
+                int sample = temp%10;
+                digitSum += sample;
+                temp /= 10;
+            }
+        }
+        return elementSum - digitSum;
+    }
+    /**
     static int differenceOfSum(int[] arr){
         return elementSum(arr) - digitSum(arr);
     }
@@ -28,4 +48,5 @@ public class DigitElement {
         }
         return result;
     }
+    **/
 }
