2a5f526009d6faf4d637b0ecb64a4729214fd516
yokesh-kumar-M <yokeshkumar1704@gmail.com>
2026-01-19 23:59:42 +0530

    Palindromic substirng

---
+++ PalindromicSubstring.java
@@ -0,0 +1,26 @@
+public class PalindromicSubstring {
+    public static void main(String[] args) {
+        String s = "aaa";
+        System.out.println(countSubString(s));
+    }
+    static int countSubString(String s){
+
+        int count = 0;
+        for(int i=0; i<s.length(); i++){
+            count += expandCenter(s, i, i);
+            count += expandCenter(s, i, i+1);
+        }
+        return count;
+    }
+    static int expandCenter(String s, int left, int right){
+        int count = 0;
+        int length = s.length();
+
+        while(left >= 0 && right < length && s.charAt(left) == s.charAt(right)){
+            count++;
+            left--;
+            right++;
+        }
+        return count;
+    }
+}
